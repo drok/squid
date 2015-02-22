5f0bab6c08ad21f9225dc0d1c06fb7aafabe71a2
Radu Hociung <radu.git@ohmi.org>
2015-02-22 09:22:09 -0500

    Clean tree (code is in another branch)

--- src/tests/stub_http.cc
+++
@@ -1,9 +0,0 @@
-#include "squid.h"
-
-#include "HttpReply.h"
-#include "HttpRequest.h"
-
-#define STUB_API "http.cc"
-#include "tests/STUB.h"
-
-const char * httpMakeVaryMark(HttpRequest * request, HttpReply const * reply) STUB_RETVAL(NULL)
--- src/tests/stub_whois.cc
+++
@@ -1,9 +0,0 @@
-#include "squid.h"
-
-#define STUB_API "whois.cc"
-#include "tests/STUB.h"
-
-class FwdState;
-
-void whoisStart(FwdState *) STUB
-
